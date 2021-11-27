4d633a0385881c19b14154f54857fe95adb4626d
ELdawlia <ELdawlia@DESKTOP-B2TE67O>
2021-11-27 03:54:06 +0200

    Add project files.

---
+++ Remove Doubly Linked List/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+//a class called program is defined
+public class program
+{
+    // Main Method is called
+    static public void Main()
+    {
+       
+        LinkedList<String> list = new LinkedList<String>();
+        //AddLast() method is used to add the elements 
+        list.AddLast("Nadeen");
+        list.AddLast("Hatem");
+        list.AddLast("El_Abd");
+       
+        Console.WriteLine("My name is:");
+       
+        foreach (string i in list)
+        {
+            Console.WriteLine(i);
+        }
+        
+        Console.WriteLine("My name after using Remove() method are:");
+        
+        list.Remove("Nadeen");
+        foreach (string i in list)
+        {
+            Console.WriteLine(i);
+        }
+        Console.WriteLine("My name after using RemoveFirst() method are:");
+        
+        list.RemoveFirst();
+        foreach (string i in list)
+        {
+            Console.WriteLine(i);
+        }
+        Console.WriteLine("my name after using RemoveLast() method are:");
+    
+        list.RemoveLast();
+        foreach (string i in list)
+        {
+            Console.WriteLine(i);
+        }
+       
+    }
+}
